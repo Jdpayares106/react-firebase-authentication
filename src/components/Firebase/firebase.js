ce57e567ee9c738e4b88a923d828fa5493e9e095
Jonathan Payares <Jdpayares@my.waketech.edu>
2019-09-11 14:59:27 -0400

    SignUP Form

--- src/components/Firebase/firebase.js
+++ src/components/Firebase/firebase.js
@@ -21,6 +21,16 @@ const config = {
       // *** Auth API ***
     doCreateUserWithEmailAndPassword = (email, password) =>
       this.auth.createUserWithEmailAndPassword(email, password);
+    
+    doSignInWithEmailAndPassword = (email, password) =>
+      this.auth.signInWithEmailAndPassword(email, password);
+
+    doSignOut = () => this.auth.signOut();
+
+    doPasswordReset = email => this.auth.sendPasswordResetEmail(email);
+    
+    doPasswordUpdate = password =>
+      this.auth.currentUser.updatePassword(password);
 
   }
   export default Firebase;
