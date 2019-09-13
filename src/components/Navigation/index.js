bb939aa74c58e2a2e66466c4856717a31ad630ca
Jonathan Payares <Jdpayares@my.waketech.edu>
2019-09-12 20:48:00 -0400

    Signin/Signout
    Set up for signing in and siging up

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -2,6 +2,9 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import * as ROUTES from '../../constants/routes';
 
+import SignOutButton from '../SignOut';
+
+
 const Navigation = () =>(
     <div>
         <ul>
@@ -19,6 +22,9 @@ const Navigation = () =>(
             </li>
             <li>
                 <Link to={ROUTES.ADMIN}>Admin</Link>
+            </li>
+            <li>
+                <SignOutButton/>
             </li>
         </ul>
     </div>
